b5726b0310aa956998c750590e43bf5b77d632a6
Claudia <claudias.mazariegos@gmail.com>
2021-06-08 15:08:04 -0700

    added yml back

--- sandbox-api/lib/app.js
+++ sandbox-api/lib/app.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.json('you are at 7890');
+  });
+
 app.use(require('./middleware/not-found'));
 app.use(require('./middleware/error'));
 
